4e6d9812b8428ced835f9f0c9555b0722b8ea5f9
edgar6bf <edgar6bf@gmail.com>
2023-11-18 02:51:07 +0900

    init project

---
+++ src/main/java/site/packit/packit/PackitApplication.java
@@ -0,0 +1,13 @@
+package site.packit.packit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/packit/packit/PackitApplicationTests.java
@@ -0,0 +1,13 @@
+package site.packit.packit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
