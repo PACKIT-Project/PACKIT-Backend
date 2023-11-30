e555df4182d8d39f8ecd3ce4bbcc9b17de3d1b72
edgar6bf <edgar6bf@gmail.com>
2023-11-30 13:37:39 +0900

    fix: #3 MemberException 에러 메시지 설정 로직 누락 수정

--- src/main/java/site/packit/packit/domain/member/exception/MemberException.java
+++ src/main/java/site/packit/packit/domain/member/exception/MemberException.java
@@ -7,6 +7,7 @@ public class MemberException extends RuntimeException {
     private final ErrorCode errorCode;
 
     public MemberException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
         this.errorCode = errorCode;
     }
 
