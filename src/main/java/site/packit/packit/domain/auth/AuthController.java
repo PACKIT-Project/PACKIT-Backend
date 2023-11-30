28962087c239fefe6ea7a9a6693898579787def7
edgar6bf <edgar6bf@gmail.com>
2023-11-30 13:38:54 +0900

    feat: #3 로그아웃 기능 구현

--- src/main/java/site/packit/packit/domain/auth/AuthController.java
+++ src/main/java/site/packit/packit/domain/auth/AuthController.java
@@ -1,17 +1,18 @@
 package site.packit.packit.domain.auth;
 
-import org.springframework.http.HttpStatus;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.packit.packit.domain.auth.dto.request.LoginRequest;
 import site.packit.packit.domain.auth.dto.response.LoginResponse;
 import site.packit.packit.domain.auth.service.AuthService;
 import site.packit.packit.domain.auth.service.TokenService;
 import site.packit.packit.global.response.success.SingleSuccessApiResponse;
+import site.packit.packit.global.response.success.SuccessApiResponse;
 import site.packit.packit.global.response.util.ResponseUtil;
+
+import static org.springframework.http.HttpStatus.OK;
 
 @RequestMapping("/api/auth")
 @RestController
@@ -29,6 +30,13 @@ public class AuthController {
     public ResponseEntity<SingleSuccessApiResponse<LoginResponse>> login(@RequestBody LoginRequest request) {
         LoginResponse loginResult = authService.login(request);
 
-        return ResponseUtil.successApiResponse(HttpStatus.OK, "성공적으로 로그인 되었습니다.", loginResult);
+        return ResponseUtil.successApiResponse(OK, "성공적으로 로그인 되었습니다.", loginResult);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<SuccessApiResponse> logout(HttpServletRequest request, HttpServletResponse response) {
+        authService.logout(request, response);
+
+        return ResponseUtil.successApiResponse(OK, "성공적으로 로그아웃 되었습니다.");
     }
 }
